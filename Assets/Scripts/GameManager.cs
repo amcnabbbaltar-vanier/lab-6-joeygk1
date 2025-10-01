c197ed62279719a63cc270a3f43947bf286843d7
Joey <joey.ayoubdisalvo@icloud.com>
2025-10-01 12:37:49 -0400

    lab 6 not done
    not done

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,42 +1,40 @@
 using UnityEngine;
 using UnityEngine.UI;
-
+using UnityEngine.SceneManagement; // Add this namespace
 
 public class GameManager : MonoBehaviour
 {
-    public static GameManager Instance { get; private set; }
+    public static GameManager Instance;
 
-
-    public Text scoreText;
-    private int score = 0;
-
+    private int totalScore = 0;
 
     private void Awake()
     {
-        // Enforce one instance
-        if (Instance != null && Instance != this)
-        {
+        if (Instance == null)
+            Instance = this;
+        else
             Destroy(gameObject);
-            return;
-        }
-        Instance = this;
     }
-
 
     public void AddScore(int amount)
     {
-        score += amount;
-        UpdateUI();
+        totalScore += amount;
+        Debug.Log("Score: " + totalScore);
     }
 
-
-    private void UpdateUI()
+    public void LoadNextScene()
     {
-        if (scoreText != null)
+        int currentIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextIndex = currentIndex + 1;
+
+        if (nextIndex < SceneManager.sceneCountInBuildSettings)
         {
-            scoreText.text = $"Score: {score}";
+            SceneManager.LoadScene(nextIndex);
+        }
+        else
+        {
+            Debug.Log("No more scenes to load.");
         }
     }
 }
-
 
